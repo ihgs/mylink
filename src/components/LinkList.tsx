3ff062d1d9768dce71c9ee6edb13fa602b8821ed
ihgs <hgs.dev@gmail.com>
2023-02-23 22:39:21 +0900

    prettier

--- src/components/LinkList.tsx
+++ src/components/LinkList.tsx
@@ -1,70 +1,78 @@
-import { useEffect, useState } from "react"
-import { countUp, LinkData, listLinks } from "../libs/storage"
-
+import { useEffect, useState } from "react";
+import { countUp, LinkData, listLinks } from "../libs/storage";
 
 interface SearchParam {
-  title: string
-  category: string
+  title: string;
+  category: string;
 }
 
 export default function LinkList() {
-
-  const [links, setLinks] = useState<Array<LinkData>>([])
-  const [search, setSearch] = useState<SearchParam>({ title: "", category: "" })
-
+  const [links, setLinks] = useState<Array<LinkData>>([]);
+  const [search, setSearch] = useState<SearchParam>({
+    title: "",
+    category: "",
+  });
 
   useEffect(() => {
-    filtedLinks()
-  }, [search])
-
+    filtedLinks();
+  }, [search]);
 
   const clickLink = (id: string | undefined | null) => {
     if (id) {
-      countUp(id)
-      filtedLinks()
+      countUp(id);
+      filtedLinks();
     }
-  }
+  };
 
   const changeSearchInput = (target: keyof SearchParam, value: string) => {
-    const newone = { ...search }
-    newone[target] = value
-    setSearch(newone)
-  }
+    const newone = { ...search };
+    newone[target] = value;
+    setSearch(newone);
+  };
 
   const filtedLinks = () => {
-    let items = listLinks()
+    let items = listLinks();
     if (search.title || search.category) {
-      items = items.filter(item => {
+      items = items.filter((item) => {
         if (search.title) {
           if (!item.title.includes(search.title)) {
-            return false
+            return false;
           }
         }
         if (search.category) {
           if (!item.category.includes(search.category)) {
-            return false
+            return false;
           }
         }
-        return true
-      })
+        return true;
+      });
     }
-    setLinks(items)
-  }
+    setLinks(items);
+  };
 
   const linkRow = (link: LinkData) => {
     return (
       <tr key={link.id}>
-        <td><a href={link.link} onClick={() => clickLink(link.id)} target="_blank" rel="noreferrer noopener" className="link">{link.title}</a></td>
+        <td>
+          <a
+            href={link.link}
+            onClick={() => clickLink(link.id)}
+            target="_blank"
+            rel="noreferrer noopener"
+            className="link"
+          >
+            {link.title}
+          </a>
+        </td>
         <td>{link.category}</td>
         <td>{link.count}</td>
         <td>{new Date(link.createdAt!).toDateString()}</td>
       </tr>
-    )
-  }
+    );
+  };
 
   return (
     <div>
-
       <table className="table w-full">
         <thead>
           <tr>
@@ -75,21 +83,27 @@ export default function LinkList() {
           </tr>
           <tr>
             <th>
-              <input className="input input-bordered" onChange={(e) => changeSearchInput('title', e.target.value)} /></th>
+              <input
+                className="input input-bordered"
+                onChange={(e) => changeSearchInput("title", e.target.value)}
+              />
+            </th>
             <th>
-              <input className="input input-bordered" onChange={(e) => changeSearchInput('category', e.target.value)} /></th>
+              <input
+                className="input input-bordered"
+                onChange={(e) => changeSearchInput("category", e.target.value)}
+              />
+            </th>
             <th></th>
             <th></th>
           </tr>
         </thead>
         <tbody>
           {links.map((link) => {
-            return linkRow(link)
-          }
-          )}
+            return linkRow(link);
+          })}
         </tbody>
       </table>
     </div>
-
-  )
+  );
 }
--- src/libs/storage.tsx
+++ src/libs/storage.tsx
@@ -1,57 +1,50 @@
-const base64 = require('base-64');
-const utf8 = require('utf8')
+const base64 = require("base-64");
+const utf8 = require("utf8");
 
 export interface LinkData {
-  id?: string|null,
-  title: string,
-  link: string,
-  category: string,
-  count?: number,
-  createdAt?: number,
+  id?: string | null;
+  title: string;
+  link: string;
+  category: string;
+  count?: number;
+  createdAt?: number;
 }
 
 const genId = (link: string) => {
-  
   return base64.encode(utf8.encode(link));
-}
+};
 
 const addLink = (data: LinkData) => {
-  const items = listLinks()
+  const items = listLinks();
   data.id = genId(data.link);
   data.createdAt = new Date().getTime();
-  if(items.find(item=> item.id === data.id)){
-    alert('already existing')
+  if (items.find((item) => item.id === data.id)) {
+    alert("already existing");
     return;
   }
-  items.push(data)
-  localStorage.setItem("items", JSON.stringify(items))
-}
+  items.push(data);
+  localStorage.setItem("items", JSON.stringify(items));
+};
 
 const countUp = (id: string) => {
-  const items = listLinks()
-  const updateItem = items[items.findIndex(x=>x.id === id)] 
-  if(updateItem['count']){
-    updateItem['count'] = updateItem['count'] + 1
-  }else {
-    updateItem['count'] = 1
+  const items = listLinks();
+  const updateItem = items[items.findIndex((x) => x.id === id)];
+  if (updateItem["count"]) {
+    updateItem["count"] = updateItem["count"] + 1;
+  } else {
+    updateItem["count"] = 1;
   }
 
-  localStorage.setItem("items", JSON.stringify(items))
-}
+  localStorage.setItem("items", JSON.stringify(items));
+};
 const listLinks = (): Array<LinkData> => {
-  let saved = localStorage.getItem("items")
+  let saved = localStorage.getItem("items");
   let items: Array<LinkData>;
-  if(!saved){
-    items = [
-     
-    ]
-  }else {
-    items = JSON.parse(saved)
+  if (!saved) {
+    items = [];
+  } else {
+    items = JSON.parse(saved);
   }
   return items;
-}
-export {
-  addLink,
-  listLinks,
-  countUp,
-}
+};
+export { addLink, listLinks, countUp };
