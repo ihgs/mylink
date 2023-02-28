910d1e7509c75738091c4bf2eb83fed131868d96
ihgs <hgs.dev@gmail.com>
2023-02-28 22:06:21 +0900

    modify link

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import './App.css';
 import Dashboard from './Dashboard';
 import Home from './Home';
 
-
+const app_path = process.env.dev || "/mylink"
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/test" element={<Dashboard />} />
+        <Route path={`${app_path}/`} element={<Home />} />
+        <Route path={`${app_path}/test`}  element={<Dashboard />} />
       </Routes>
     </div>
   )
