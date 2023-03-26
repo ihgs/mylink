a52925e62a78f3965ac91dde94f4f31fb8a33396
ihgs <hgs.dev@gmail.com>
2023-03-26 19:58:11 +0900

    update

---
+++ src/LinkManager.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import LinkList from './components/LinkList';
+import NewLink from './components/NewLink';
+import Title from './components/Title';
+
+function LinkManager() {
+  const [reload, setReload] = useState<any>({})
+
+  const postSave = () => {
+    setReload({})
+  }
+
+  return (
+    <div>
+      <Title title="Manage Link" />
+      <LinkList realod={reload} />
+      <NewLink postSave={postSave}/>
+    </div>
+  );
+}
+
+export default LinkManager;
