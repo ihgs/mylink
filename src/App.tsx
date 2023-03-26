7fabb834ce1d0f6b64a74cf24f6ea8f4354abffe
ihgs <hgs.dev@gmail.com>
2023-03-26 22:39:56 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,12 @@ import Header, { Menu } from './components/Header';
 import Dashboard from './Dashboard';
 import LinkManager from './LinkManager';
 
-const app_path = process.env.dev || "/mylink"
 
 function App() {
   const menus: Array<Menu> = [
-    {link: `${app_path}/`, title: "Home"},
-    {link: `${app_path}/linkmanager`, title: "Manage Link"},
-    {link: `${app_path}/categories`, title: "Category"},
+    {link: `/`, title: "Home"},
+    {link: `/linkmanager`, title: "Manage Link"},
+    {link: `/categories`, title: "Category"},
   ] 
 
   return (
@@ -19,9 +18,9 @@ function App() {
       <Header menus={menus} />
     <div className="App w-5/6 mx-auto">
       <Routes>
-        <Route path={`${app_path}/linkmanager`} element={<LinkManager />} />
-        <Route path={`${app_path}/`}  element={<Dashboard />} />
-        <Route path={`${app_path}/categories`}  element={<CategoryManager />} />
+        <Route path={`/linkmanager`} element={<LinkManager />} />
+        <Route path={`/`}  element={<Dashboard />} />
+        <Route path={`/categories`}  element={<CategoryManager />} />
       </Routes>
     </div>
     </div>
